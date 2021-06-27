1c54e6d4c936ca2c2552209b9478f6ddb12d9d41
Andres Nodas <nodasiniesta@gmail.com>
2021-06-27 16:05:28 +0700

    Membuat test

---
+++ src/main/java/programmer/zaman/now/unit/test/App.java
@@ -0,0 +1,13 @@
+package programmer.zaman.now.unit.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
