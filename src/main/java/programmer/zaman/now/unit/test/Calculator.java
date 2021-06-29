e83681fa119f8acbbed4ea0cc0baf928634f84e8
Andres Nodas <nodasiniesta@gmail.com>
2021-06-29 07:39:51 +0700

    Merubah nama test

--- src/main/java/programmer/zaman/now/unit/test/Calculator.java
+++ src/main/java/programmer/zaman/now/unit/test/Calculator.java
@@ -2,6 +2,17 @@ package programmer.zaman.now.unit.test;
 
 public class Calculator {
 
+	public Integer divide(Integer first, Integer second) {
+		
+		if(second == 0) {
+			throw new IllegalArgumentException("Cannot divide by zero");
+		}
+		else {
+			return first / second;
+		}
+		
+	}
+	
 	public Integer add(Integer first, Integer second) {
 		return first + second;
 	}
--- src/test/java/programmer/zaman/now/unit/test/CalculatorTest.java
+++ src/test/java/programmer/zaman/now/unit/test/CalculatorTest.java
@@ -1,14 +1,41 @@
 package programmer.zaman.now.unit.test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.Test;
 
+import programmer.zaman.now.unit.test.generator.SimpleDisplayNameGenerator;
+
+//@DisplayName("Test Calculator")
+@DisplayNameGeneration(value = SimpleDisplayNameGenerator.class)
 public class CalculatorTest {
 
 	private Calculator calculator = new Calculator();
 	
 	@Test
+//	@DisplayName("Test Function Calculator.add(Integer, Integer)")
 	public void testAddSuccess() {
 		Integer result = calculator.add(10, 10);
+		
+		assertEquals(20, result);
+	}
+	
+	@Test
+	public void testDivideSuccess() {
+		Integer result = calculator.divide(10, 2);
+		
+		assertEquals(5, result);
+	}
+	
+	@Test
+	public void testDivideThrowException() {
+		
+		assertThrows(IllegalArgumentException.class, () -> {
+			calculator.divide(10, 0);
+		});
+		
 	}
 	
 }
